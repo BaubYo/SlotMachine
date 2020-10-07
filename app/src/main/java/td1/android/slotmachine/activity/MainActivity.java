b4b8ccb3e3b724ab4ceca0674ddb7dfbfbbf701c
fendi74960 <serpiente61@hotmail.fr>
2020-10-07 10:04:14 +0200

    Start la view MainActivity plus lien avec le button qui tire les choix

--- app/src/main/java/td1/android/slotmachine/activity/MainActivity.java
+++ app/src/main/java/td1/android/slotmachine/activity/MainActivity.java
@@ -2,6 +2,8 @@ package td1.android.slotmachine.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import td1.android.slotmachine.R;
@@ -12,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Onclick sur le button levier envoie les themes ( A FAIRE ) et affiche la vue ChoiceActivity
+        findViewById(R.id.MainSlotButton).setOnClickListener((v)-> {
+            Intent intent = new Intent(getApplicationContext(),ChoiceActivity.class);
+            //intent.putExtra(EXTRA_CONTACT,contacts.get(list.getChildViewHolder(v).getAdapterPosition()));
+            //FAUDRA DONNER LES TROIS THEMES
+            startActivity(intent);
+        });
     }
+
+
 }
