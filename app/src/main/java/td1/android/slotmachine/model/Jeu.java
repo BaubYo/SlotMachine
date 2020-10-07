cf3f4e2e8645661dc8434b52ee1fb04659df78d2
BaubYo <66359241+BaubYo@users.noreply.github.com>
2020-10-07 10:07:58 +0200

    Modification de la classe Jeu + Ajout d'id dans les activités Choice et Information

--- app/src/main/java/td1/android/slotmachine/model/Jeu.java
+++ app/src/main/java/td1/android/slotmachine/model/Jeu.java
@@ -3,6 +3,53 @@ package td1.android.slotmachine.model;
 import java.util.List;
 
 public class Jeu {
+
+    //Create Getter, Setter, Constructor : Alt+Insert
+
+    //Atributs
     private List<Theme> themes;
-    private String name;
+    private String nom;
+    private int annee;
+    private String resume;
+
+    //Constructeur
+    public Jeu(List<Theme> themes, String nom, int annee, String resume) {
+        this.themes = themes;
+        this.nom = nom;
+        this.annee = annee;
+        this.resume = resume;
+    }
+
+    //Getter et Setter
+    public List<Theme> getThemes() {
+        return themes;
+    }
+
+    public void setThemes(List<Theme> themes) {
+        this.themes = themes;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
 }
