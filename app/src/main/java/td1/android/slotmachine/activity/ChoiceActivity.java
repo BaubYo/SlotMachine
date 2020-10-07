9c29b13698c5b06236efd91198a22d343e71c6cf
BaubYo <66359241+BaubYo@users.noreply.github.com>
2020-10-07 10:19:25 +0200

    Modification du gradle (7 -> 8)

--- app/src/main/java/td1/android/slotmachine/activity/ChoiceActivity.java
+++ app/src/main/java/td1/android/slotmachine/activity/ChoiceActivity.java
@@ -2,15 +2,25 @@ package td1.android.slotmachine.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import td1.android.slotmachine.R;
+import td1.android.slotmachine.model.Jeu;
 
 public class ChoiceActivity extends AppCompatActivity {
+    private Jeu jeu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
+
+        //Pour renlancer le jeu
+        findViewById(R.id.choice_another).setOnClickListener((v) -> {
+            //TODO masse code ici !
+            jeu = new Jeu();
+        });
     }
 }
