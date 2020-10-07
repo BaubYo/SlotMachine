fde0c9f0a5c0714ab760917a13a8c06e4f2a6b7e
fendi74960 <serpiente61@hotmail.fr>
2020-10-07 10:36:56 +0200

    Start la view MainActivity plus lien avec le button qui tire les choix

--- app/src/main/java/td1/android/slotmachine/model/Theme.java
+++ app/src/main/java/td1/android/slotmachine/model/Theme.java
@@ -1,5 +1,43 @@
 package td1.android.slotmachine.model;
 
 public class Theme {
+
+
     private String name;
+    private String color;
+
+    //Constructeur
+    public Theme(String name){
+        this.name=name;
+
+        switch(name.toUpperCase()) {
+            case "RPG":
+                this.color="@colors/jaune";
+                break;
+            case "RTS":
+                this.color="@colors/bleu";
+                break;
+            case "MOBA":
+                this.color="@colors/rouge";
+                break;
+            case "TACTICAL":
+                this.color="@colors/vert";
+                break;
+            default:
+                this.color="@colors/blanc";
+        }
+
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
