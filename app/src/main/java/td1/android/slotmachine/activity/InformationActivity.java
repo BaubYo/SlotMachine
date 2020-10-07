274da1d1a1c9278797eb1baee49a6802cd1c7cbe
BaubYo <66359241+BaubYo@users.noreply.github.com>
2020-10-07 09:53:36 +0200

    Création de deux activity InformationActivity + ChoiceActivity

---
+++ app/src/main/java/td1/android/slotmachine/activity/InformationActivity.java
@@ -0,0 +1,16 @@
+package td1.android.slotmachine.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import td1.android.slotmachine.R;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
